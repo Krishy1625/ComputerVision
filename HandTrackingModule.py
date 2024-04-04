76a4d2c0ca8c01abe3f096009d65ff3d0e3c58e8
Krishy1625 <Krishp038@gmail.com>
2024-04-04 23:48:34 +0100

    add a find position methon in the tracking module

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -25,13 +25,20 @@ class handDetector():
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
-                # for id, lm in enumerate(handLms.landmark):
-                #     #print(id,lm)
-                #     h, w, c = img.shape
-                #     cx, cy = int(lm.x*w), int(lm.y*h)
-                #     print(id, cx, cy)
-                #     if id == 4:
-                #         cv2.circle(img, (cx,cy), 15, (255,0,255),cv2.FILLED)
+    
+    def findPosition(self, img, hanNo = 0, draw = True):
+        
+        lmList = []
+        
+        for id, lm in enumerate(self.handLms.landmark):
+            #print(id,lm)
+            h, w, c = img.shape
+            cx, cy = int(lm.x*w), int(lm.y*h)
+            print(id, cx, cy)
+            if id == 4:
+                cv2.circle(img, (cx,cy), 15, (255,0,255),cv2.FILLED)
+                
+        return lmList
             
     
 def main():
